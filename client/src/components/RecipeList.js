464eba485ddf43843ebc5aaaf25be571af78bbd4
IndupriyaVempati <vempatiindu744@gmail.com>
2025-03-28 02:01:20 +0530

    Modified files

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -92,7 +92,7 @@ const RecipeList = () => {
             <div className="card h-100">
               {recipe.image && (
                 <img
-                  src={recipe.image}
+                  src={`https://flavoshare.onrender.com${recipe.image}`}
                   alt={recipe.title}
                   className="card-img-top"
                   style={{ height: "200px", objectFit: "cover" }}
